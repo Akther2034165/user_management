9cf60a472f3c23f787a9fa4261a503a7bb17e4bb
Md Akther Hosen <dev.aktherhosen@gmail.com>
2024-02-02 19:32:50 +0600

    updated

--- user_management/settings.py
+++ user_management/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-z#7g&yekrb1wm!g+-c5ol(!8hff1dv*5u2scf6kly0^bnh2#-g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
